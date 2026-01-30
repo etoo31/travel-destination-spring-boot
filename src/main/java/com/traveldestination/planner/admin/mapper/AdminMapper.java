5e3f94d4a3f761ec1b9103ef10d0e6d928bbb3ae
Youssef Ashraf <youssefashrafeissa@gmail.com>
2026-01-30 19:35:47 +0200

    update: add admin mapper for mapping diffrent objects

---
+++ src/main/java/com/traveldestination/planner/admin/mapper/AdminMapper.java
@@ -0,0 +1,20 @@
+package com.traveldestination.planner.admin.mapper;
+
+import com.traveldestination.planner.common.model.Destination;
+import com.traveldestination.planner.common.model.entity.ApprovedDestinationsEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AdminMapper {
+
+    public ApprovedDestinationsEntity map(Destination destination) {
+        return ApprovedDestinationsEntity.builder()
+                .country(destination.getCountry())
+                .capital(destination.getCapital())
+                .flag(destination.getFlagUrl())
+                .currency(destination.getCurrency())
+                .population(destination.getPopulation())
+                .region(destination.getRegion())
+                .build();
+    }
+}
--- src/main/java/com/traveldestination/planner/admin/service/AdminService.java
+++ src/main/java/com/traveldestination/planner/admin/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.traveldestination.planner.admin.service;
 
+import com.traveldestination.planner.admin.mapper.AdminMapper;
 import com.traveldestination.planner.common.model.Destination;
 import com.traveldestination.planner.common.model.entity.ApprovedDestinationsEntity;
 import com.traveldestination.planner.common.respository.facade.ApprovedDestinationsRepositoryFacade;
@@ -12,19 +13,13 @@ import org.springframework.stereotype.Service;
 public class AdminService {
     private final FavoriteDestinationsRepositoryFacade favoriteDestinationsRepositoryFacade;
     private final ApprovedDestinationsRepositoryFacade approvedDestinationsRepositoryFacade;
+    private final AdminMapper adminMapper;
     public void approveDestination(long userId, Destination destination) {
         // Remove from favorite destinations if exists
         favoriteDestinationsRepositoryFacade.deleteByCountry(destination.getCountry());
 
         // Add to approved destinations
-        ApprovedDestinationsEntity approvedDestinationsEntity = ApprovedDestinationsEntity.builder()
-                .country(destination.getCountry())
-                .capital(destination.getCapital())
-                .flag(destination.getFlagUrl())
-                .currency(destination.getCurrency())
-                .population(destination.getPopulation())
-                .region(destination.getRegion())
-                .build();
+        ApprovedDestinationsEntity approvedDestinationsEntity = adminMapper.map(destination);
 
         approvedDestinationsRepositoryFacade.save(approvedDestinationsEntity);
 
